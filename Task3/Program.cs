5dc4e8aac37b91f3c18c68aa2f333bee9e9b18cc
chp_h <veron.grigoreva@gmail.com>
2023-12-14 02:50:28 +0300

    Task3 is done

---
+++ Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задайте произвольный массив. Выведете
+//его элементы, начиная с конца. Использовать
+//рекурсию, не использовать циклы.
+
+
+void InputArray(int[] array)
+{
+    for (int index = 0; index < array.Length; index++)
+    {
+        array[index] = new Random().Next(1, 10);
+
+    }
+    Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+
+}
+void ArrayReverse(int[] array, int leftIndex, int rightIndex)
+{
+   
+    if (leftIndex < rightIndex)
+    {
+        int temporary = array[leftIndex];
+        array[leftIndex] = array[rightIndex];
+        array[rightIndex] = temporary;
+        ArrayReverse(array, leftIndex + 1, rightIndex - 1);
+    }
+
+}
+int n = new Random().Next(1, 10);
+int[] array = new int[n];
+InputArray(array);
+int index = 0;
+ArrayReverse(array, index, array.Length - 1);
+Console.WriteLine($"Перевернутый массив: [{string.Join(", ", array)}]");
