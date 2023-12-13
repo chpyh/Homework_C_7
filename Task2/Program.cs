3d7a2d26ded18adad46ed685262ac1583a7697a8
chp_h <veron.grigoreva@gmail.com>
2023-12-14 01:46:34 +0300

    Task2 is done

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Напишите программу вычисления функции Аккермана 
+//с помощью рекурсии. Даны два неотрицательных числа m и n.
+//А(0,n)= n+1;
+//A(m,0)= A(m-1,1);
+//A(m,n)= A(m-1, A(m,(n-1)));
+// поставила пределы небольшие, поскольку даже с ними пишет, что "stack overflow". 
+//не рискнула запросить с консоли, проверить невозможно)
+
+
+int АckFunction (int m, int n)
+{
+    if(m == 0)
+    {
+        return n + 1;
+    }
+    else if(n == 0)
+    {
+        return АckFunction(m - 1, 1);
+    }
+    
+        return АckFunction(m - 1, АckFunction(m, (n - 1)));
+}
+
+int m = new Random().Next(1, 6); 
+Console.WriteLine(m);
+int n = new Random().Next(1, 6);
+Console.WriteLine(n);
+Console.WriteLine(АckFunction(m, n));
