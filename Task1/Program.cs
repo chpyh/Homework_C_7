1b688800aa8ccb18edf46e0f56c1c0ebc6a9d57b
chp_h <veron.grigoreva@gmail.com>
2023-12-14 01:00:48 +0300

    Task1 is done

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 1: Задайте значения M и N. Напишите программу, которая 
+//выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы.
+
+
+
+void printNumbersBetweenMAndN(int m, int n)
+{
+    if(m <= n)
+    {
+        Console.Write ($"{m + " "}");
+        printNumbersBetweenMAndN(m + 1, n);}
+    return;
+    
+}
+
+Console.WriteLine("Введите натуральное число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число побольше: ");
+int n = int.Parse(Console.ReadLine()!);
+printNumbersBetweenMAndN (m, n);
